387da86bda103085e398d89491a96b9fb6e9eb34
lennijusten <ljusten@wisc.edu>
2022-11-02 17:07:04 -0400

    Add additional room specs

--- exposure_dose.py
+++ exposure_dose.py
@@ -5,21 +5,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-# Fixed
-h_room = 2.46  # m
+# Person dimensions
 h_stand = 1.83  # m (assuming 6ft individual)
 h_sit = 1.42  # m (assuming 6ft individual)
-r_stand = 0.61  # m (assuming 2ft radius from lamp center)
-r_sit = 0.61  # m (assuming 2ft radius from lamp center)
+
+# Office dimensions
+h_office = 2.46  # height of small office room (m)
+r_office = 0.61  # distance from vertical lamp axis (m). Varies on room-by-room basis.
+
+# Meeting room dimensions
+h_mroom = h_office  # same height as small offices (m)
+r_mroom = 1.23  # max distance from center table (m)
+
+# Kitchen
+h_kibeam = 2.57  # height of I-beams in the kitchen (m)
+h_kceil = 3.05  # height kitchen ceiling (m)
+r_ibeam = 2.29  # distance between I-beams (m)
 
 # Lamp sepcs at peak irradiance (r=0). See
 # https://www.ushio.com/files/specifications/care222-filtered-far-uv-c-excimer-lamp-module-technical-data-sheet.pdf
 p_narrow = np.array([14, 6.2, 2.2])  # Ushio Care222 B1 (narrow) specs (mW/cm^2).
 p_wide = np.array([1.9, 0.8, 0.3])  # Ushio Care222 B1.5 (wide) specs(mW/cm^2)
 
-# Variable
+# Fit intensity curve to Ushio irradiance data
 h = np.array([1, 1.5, 2.5])  # vertical distance from lamp center (m)
-r = 0
+r = 0  # peak irradiance
 
 # For peak irradiation, r=0, so distance from lamp d = h
 d = np.sqrt(r ** 2 + h ** 2)
@@ -51,6 +61,8 @@ plt.legend()
 plt.show()
 
 
+# Exposure calculations
+
 def exposure_dose(r, h, t, lamp='B1'):
     # r = horizontal distance from lamp center (m)
     # h = vertical distance from lamp center (m)
@@ -58,28 +70,28 @@ def exposure_dose(r, h, t, lamp='B1'):
     # lamp {B1, B.15} where B1 is narrow and B1.5 is wide
 
     if lamp == 'B1':
-        P = 175.76/1000  # Lamp power from fit
+        P = 175.76 / 1000  # Lamp power from fit
     elif lamp == 'B1.5':
-        P = 23.67/1000
+        P = 23.67 / 1000
     else:
         return 'Lamp code is invalid {B1, B1.5}'
 
     # calculate irradiance
-    d = np.sqrt(r**2 + h**2)
+    d = np.sqrt(r ** 2 + h ** 2)
     I = intensity(d, P)
 
     # calculate dose (mJ/cm^2)
-    return I * (t*60*60)
+    return I * (t * 60 * 60)
 
 
 # 8h exposure dose for the head of a 6ft tall person standing at peak irradiance in small office under B1 lamp
-dose1 = exposure_dose(0, h_room-h_stand, 8, lamp='B1')
+dose1 = exposure_dose(0, h_office - h_stand, 8, lamp='B1')
 
 # 8h exposure dose for the head of a 6ft tall person sitting at peak irradiance in small office under B1 lamp
-dose2 = exposure_dose(0, h_room-h_sit, 8, lamp='B1')
+dose2 = exposure_dose(0, h_office - h_sit, 8, lamp='B1')
 
 # 8h exposure dose for the head of a 6ft tall person standing in small office under center-ceiling B1 lamp
-dose3 = exposure_dose(r_stand, h_room-h_stand, 8, lamp='B1')
+dose3 = exposure_dose(r_office, h_office - h_stand, 8, lamp='B1')
 
 # 8h exposure dose for the head of a 6ft tall person sitting in small office under center-ceiling B1 lamp
-dose4 = exposure_dose(r_sit, h_room-h_sit, 8, lamp='B1')
+dose4 = exposure_dose(r_office, h_office - h_sit, 8, lamp='B1')
